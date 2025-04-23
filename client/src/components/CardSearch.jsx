3455f3bfe2f87db3e11e948a5e5a76b138ce42f3
Brandon J. Hernandez <hernandezbrandon17@gmail.com>
2025-04-23 14:58:01 -0500

    Fix image source in CardSearch component to use PNG for better quality

--- client/src/components/CardSearch.jsx
+++ client/src/components/CardSearch.jsx
@@ -43,7 +43,7 @@ const CardSearch = () => {
           <div key={card.id} className="card-container">
             {card.image_uris ? (
               <img
-                src={card.image_uris.large} // Use 'large' for high-quality images
+                src={card.image_uris.png} // Use 'png' for high-quality images
                 alt={card.name}
                 className="card-image"
               />
