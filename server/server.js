1bbbaac0506fdbf55e9e939424424d38965e14dc
Brandon J. Hernandez <hernandezbrandon17@gmail.com>
2025-04-24 12:39:02 -0500

    feat: add helmet for enhanced security and update package dependencies

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const helmet = require("helmet");
 
 dotenv.config(); // Load environment variables from .env file
 
 const app = express();
+app.use(helmet()); // Add Helmet middleware to enhance security
+
 const PORT = process.env.PORT || 5000; // Use PORT from .env or default to 5000
 
 app.get("/", (req, res) => {
