a886836c2c2083aeec9449944760a3e17cd91ae9
Brandon J. Hernandez <hernandezbrandon17@gmail.com>
2025-04-24 10:52:45 -0500

    Refactor NavBar styles for improved layout and fixed positioning

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import NavBar from "./components/NavBar";
 const App = () => {
   return (
     <>
-      <NavBar /> {/* Add the NavBar here */}
+      <NavBar />
       <CardSearch />
     </>
   );
