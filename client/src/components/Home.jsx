6a4b39af7ed20f13a7ecc229f4305fb682aaf9bf
Brandon J. Hernandez <hernandezbrandon17@gmail.com>
2025-04-24 11:04:25 -0500

    Add Home component and corresponding CSS styles

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import CardSearch from "./components/CardSearch";
+import Home from "./components/Home"; // Import the new Home component
+
+const App = () => {
+  return (
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} /> {/* Home page */}
+        <Route path="/search" element={<CardSearch />} /> {/* Search page */}
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
